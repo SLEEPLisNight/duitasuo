e0cab8b63dd075eb761a44a80e332b8e8e67a4d2
Michael Meyer <m.meyer2k@gmail.com>
2013-07-26 13:51:43 -0500

    Prevent SQL injection in several parameters

--- insertgifts.php
+++ insertgifts.php
@@ -1,7 +1,7 @@
 ﻿<?php
 include 'connect_to_mysql.php';
-$name = $_GET['name'];
-$descr = $_GET['descr'];
-$price = $_GET['price'];
+$name = mysql_real_escape_string($_GET['name']);
+$descr = mysql_real_escape_string($_GET['descr']);
+$price = mysql_real_escape_string($_GET['price']);
 mysql_query("INSERT INTO gifts (name,descr,price) VALUES('$name','$descr','$price')") or die(mysql_error()); 
 ?>
